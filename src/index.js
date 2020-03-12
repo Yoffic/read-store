89745ae8e7a183b8b8a7575d479b33af0f89ea4f
Olga Ioffe <yoffic@yahoo.com>
2020-03-12 13:23:24 +0300

    add services to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import ErrorBoundary from './components/error-boundary';
+import BookstoreService from './services/bookstore-service';
+import { BookstoreServiceProvider } from './components/bs-service-context';
 
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store';
+
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundary>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+          <App />
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoundary>
+  </Provider>,
+  document.getElementById('root')
+);
