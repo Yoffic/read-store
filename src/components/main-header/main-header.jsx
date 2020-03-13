b5d1e4efef2bc59be675d9e1e4b31d424d32e893
Olga Ioffe <yoffic@yahoo.com>
2020-03-13 14:21:12 +0300

    fix links

--- src/components/main-header/main-header.jsx
+++ src/components/main-header/main-header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './main-header.css';
 
 const MainHeader = ({ numItems, total }) => {
   return (
     <header className="main-header row">
-      <a className="logo text-dark" href="/">ReadStore</a>
-      <a className="shopping-cart" href="/">
-        <i className="cart-icon fa fa-shopping-cart" />
-        {numItems} items (${total})
-      </a>
+      <Link to=''>
+        <div className="logo text-dark">ReadStore</div>
+      </Link>
+      <Link to="/cartpage">
+        <div className="shopping-cart">
+          <i className="cart-icon fa fa-shopping-cart" />
+          {numItems} items (${total})
+        </div>
+      </Link>
     </header>
   );
 };
