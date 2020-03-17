61cf664df8cae7eec67d9adba439e5600f2ed07a
Olga Ioffe <yoffic@yahoo.com>
2020-03-17 22:05:09 +0300

    add data

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -14,7 +14,35 @@ export default class BookstoreService {
       author: 'Michael T. Nygard',
       price: 45,
       coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg',
-    }
+    },
+    {
+      id: 3,
+      title: 'Eloquent JavaScript',
+      author: 'Marijn Haverbeke',
+      price: 25,
+      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51-5ZXYtcML._SX377_BO1,204,203,200_.jpg',
+    },
+    {
+      id: 4,
+      title: 'Code Complete',
+      author: 'Steve McConnell',
+      price: 30,
+      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/514Fchkr9WL._SX408_BO1,204,203,200_.jpg',
+    },
+    {
+      id: 5,
+      title: 'The Linux Command Line',
+      author: 'William Shotts',
+      price: 26,
+      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51-T4ZwKduL._SX376_BO1,204,203,200_.jpg',
+    },
+    {
+      id: 6,
+      title: 'Docker: Up & Running',
+      author: 'Sean P. Kane',
+      price: 31,
+      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/510qnZZj0sL._SX379_BO1,204,203,200_.jpg',
+    },
   ];
 
   getBooks() {
