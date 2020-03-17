a7516c70e193f38bb4937fb51a225619ef53d856
Olga Ioffe <yoffic@yahoo.com>
2020-03-18 00:31:59 +0300

    Updates

---
+++ precache-manifest.781d93c8dfe87f4f1bd2691665027e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd52163e9d790e330a527942f43559e8",
+    "url": "/read-store/index.html"
+  },
+  {
+    "revision": "5faea397d8a6cd34fc35",
+    "url": "/read-store/static/css/main.75b63ff6.chunk.css"
+  },
+  {
+    "revision": "b4177e60389adf2b0229",
+    "url": "/read-store/static/js/2.7a599454.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/read-store/static/js/2.7a599454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5faea397d8a6cd34fc35",
+    "url": "/read-store/static/js/main.5fab6c68.chunk.js"
+  },
+  {
+    "revision": "b3a622198b5344aa856b",
+    "url": "/read-store/static/js/runtime-main.a8555e41.js"
+  }
+]);
